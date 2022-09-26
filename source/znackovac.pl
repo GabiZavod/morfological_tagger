6c8e20e035dc14859aa8d898950ae9f2d4e5018a
Gabi <gabikaaz@gmail.com>
2022-09-26 14:11:43 +0200

    Add rytmické krátenie, correct negation

--- source/znackovac.pl
+++ source/znackovac.pl
@@ -77,14 +77,23 @@ srcp([i,a,m], [n,s,p,d]). % srdcIAM, visvedčenIAM
 % Koncovka = ám
 srcp([á,m], [n,z,p,d]). % zenÁM
 srcp([á,m], [n,s,p,d]). % mestÁM
+% Koncovka = am
+srcp([a,m], [n,z,p,d]). % zenÁM (rytmické krátenie - čiarkam)
+srcp([a,m], [n,s,p,d]). % mestÁM (rytmické krátenie - miestam)
 % Koncovka = iach
 srcp([i,a,c,h], [n,z,p,l]). % ulicIAMI, dlanIACH, kostIACH
 % Koncovka = ách
 srcp([á,c,h], [n,z,p,l]). % zenÁCH
 srcp([á,c,h], [n,s,p,l]). % mestÁCH
+% Koncovka = ach
+srcp([a,c,h], [n,z,p,l]). % zenÁCH (rytmické krátenie - čiarkach)
+srcp([a,c,h], [n,s,p,l]). % mestÁCH (rytmické krátenie - miestach)
 % Koncovka = á 
 srcp([á], [n,s,p,n]). % mestÁ
 srcp([á], [n,s,p,a]). % mestÁ
+% Koncovka = a
+srcp([a], [n,s,p,n]). % mestÁ (miesta)
+srcp([a], [n,s,p,a]). % mestÁ
 % Koncovka = ia
 srcp([i,a], [n,s,p,n]). % srdcIA, vysvedčenIA
 srcp([i,a], [n,s,p,a]). % srdcIA, vysvedčenIA
@@ -129,10 +138,17 @@ srcp([x], [n,z,p,g]). % žien, ulíc
 % Koncovka = ý
 srcp([ý], [a,m,s,n]). % peknÝ
 srcp([ý], [a,m,s,a]). % peknÝ
+% Koncovka = y
+srcp([y], [a,m,s,n]). % peknÝ (rytm.k.)
+srcp([y], [a,m,s,a]). % peknÝ (rytm.k.)
 % Koncovka = á
 srcp([á], [a,z,s,n]). % peknÁ
+% Koncovka = a
+srcp([a], [a,z,s,n]). % peknÁ (rytm.k.), otcova
 % Koncovka = ú
 srcp([ú], [a,z,s,a]). % peknÚ
+% Koncovka = u
+srcp([u], [a,z,s,a]). % peknÚ (rytm.k.), otcovu
 % Koncovka = é
 srcp([é], [a,s,s,n]). % peknÉ
 srcp([é], [a,s,s,a]). % peknÉ
@@ -142,10 +158,23 @@ srcp([é], [a,s,p,a]). % peknÉ
 srcp([é], [a,m,p,a]). % peknÉ
 srcp([é], [a,z,p,n]). % peknÉ
 srcp([é], [a,z,p,a]). % peknÉ
+% Koncovka = e
+srcp([e], [a,s,s,n]). % peknÉ (r.k.)
+srcp([e], [a,s,s,a]). % peknÉ (r.k.)
+srcp([e], [a,s,p,n]). % peknÉ (r.k.), otcove
+srcp([e], [a,m,p,n]). % peknÉ (r.k.), otcove
+srcp([e], [a,s,p,a]). % peknÉ (r.k.), otcove
+srcp([e], [a,m,p,a]). % peknÉ (r.k.), otcove
+srcp([e], [a,z,p,n]). % peknÉ (r.k.), otcove
+srcp([e], [a,z,p,a]). % peknÉ (r.k.), otcove
 % Koncovka = í
 srcp([í], [a,m,s,n]). % cudzÍ
 srcp([í], [a,m,s,a]). % cudzÍ
-srcp([í], [a,m,p,n]). % peknÍ
+srcp([í], [a,m,p,n]). % peknÍ, cudzÍ
+% Koncovka = i
+srcp([i], [a,m,s,n]). % cudzÍ (r.k.)
+srcp([i], [a,m,s,a]). % cudzÍ (r.k.)
+srcp([i], [a,m,p,n]). % peknÍ, cudzÍ (r.k.), otcovi
 % Koncovka = ých
 srcp([ý,c,h], [a,m,p,g]). %peknÝCH
 srcp([ý,c,h], [a,z,p,g]). %peknÝCH
@@ -154,6 +183,14 @@ srcp([ý,c,h], [a,m,p,a]). %peknÝCH
 srcp([ý,c,h], [a,m,p,l]). %peknÝCH
 srcp([ý,c,h], [a,z,p,l]). %peknÝCH
 srcp([ý,c,h], [a,s,p,l]). %peknÝCH
+% Koncovka = ych
+srcp([y,c,h], [a,m,p,g]). %peknÝCH (r.k.)
+srcp([y,c,h], [a,z,p,g]). %peknÝCH (r.k.)
+srcp([y,c,h], [a,s,p,g]). %peknÝCH (r.k.)
+srcp([y,c,h], [a,m,p,a]). %peknÝCH (r.k.)
+srcp([y,c,h], [a,m,p,l]). %peknÝCH (r.k.)
+srcp([y,c,h], [a,z,p,l]). %peknÝCH (r.k.)
+srcp([y,c,h], [a,s,p,l]). %peknÝCH (r.k.)
 % Koncovka = ého
 srcp([é,h,o], [a,m,s,g]). % peknÉHO
 srcp([é,h,o], [a,s,s,g]). % peknÉHO
@@ -161,9 +198,16 @@ srcp([é,h,o], [a,m,s,a]). % peknÉHO
 % Koncovka = ému
 srcp([é,m,u], [a,m,s,d]). % peknÉMU
 srcp([é,m,u], [a,s,s,d]). % peknÉMU
+% Koncovka = eho
+srcp([e,h,o], [a,m,s,g]). % peknÉHO, súcEHO(r.k.)
+srcp([e,h,o], [a,s,s,g]). % peknÉHO, súcEHO(r.k.)
+srcp([e,h,o], [a,m,s,a]). % peknÉHO, súcEHO(r.k.)
+% Koncovka = emu
+srcp([e,m,u], [a,m,s,d]). % peknÉMU, súcEMU(r.k.)
+srcp([e,m,u], [a,s,s,d]). % peknÉMU, súcEMU(r.k.)
 % Koncovka = om
-srcp([o,m], [a,m,s,l]). % peknOM
-srcp([o,m], [a,s,s,l]). % peknOM
+srcp([o,m], [a,m,s,l]). % peknOM, cudzOM
+srcp([o,m], [a,s,s,l]). % peknOM, cudzOM
 % Koncovka = ým
 srcp([ý,m], [a,m,s,i]).
 srcp([ý,m], [a,s,s,i]).
@@ -174,6 +218,16 @@ srcp([ý,m], [a,s,p,d]).
 srcp([ý,m,i], [a,m,p,i]). % peknÝMI
 srcp([ý,m,i], [a,z,p,i]). % peknÝMI
 srcp([ý,m,i], [a,s,p,i]). % peknÝMI
+% Koncovka = ym
+srcp([y,m], [a,m,s,i]).
+srcp([y,m], [a,s,s,i]).
+srcp([y,m], [a,m,p,d]).
+srcp([y,m], [a,z,p,d]).
+srcp([y,m], [a,s,p,d]).
+% Koncovka = ymi
+srcp([y,m,i], [a,m,p,i]). % peknÝMI
+srcp([y,m,i], [a,z,p,i]). % peknÝMI
+srcp([y,m,i], [a,s,p,i]). % peknÝMI
 % Koncovka = ej
 srcp([e,j], [a,z,s,g]). % peknEJ, cudzEJ
 srcp([e,j], [a,z,s,d]). % peknEJ, cudzEJ
@@ -197,15 +251,18 @@ srcp([i,e,h,o], [a,s,s,g]). % cudzIEHO
 % Koncovka = iemu
 srcp([i,e,m,u], [a,m,s,d]). %cudzIEMU
 srcp([i,e,m,u], [a,s,s,d]). %cudzIEMU
-% Koncovka = om
-srcp([o,m], [a,m,s,l]). % cudzOM
-srcp([o,m], [a,s,s,l]). % cudzOM
 % Koncovka = ím
 srcp([í,m], [a,m,s,i]). % cudzÍM
 srcp([í,m], [a,s,s,i]). % cudzÍM
 srcp([í,m], [a,m,p,d]). % cudzÍM
 srcp([í,m], [a,z,p,d]). % cudzÍM
 srcp([í,m], [a,s,p,d]). % cudzÍM
+% Koncovka = im
+srcp([i,m], [a,m,s,i]). % cudzÍM (r.k.)
+srcp([i,m], [a,s,s,i]). % cudzÍM (r.k.)
+srcp([i,m], [a,m,p,d]). % cudzÍM (r.k.)
+srcp([i,m], [a,z,p,d]). % cudzÍM (r.k.)
+srcp([i,m], [a,s,p,d]). % cudzÍM (r.k.)
 % Koncovka = ích
 srcp([í,c,h], [a,m,p,g]). % cudzÍCH
 srcp([í,c,h], [a,z,p,g]). % cudzÍCH
@@ -214,27 +271,28 @@ srcp([í,c,h], [a,m,p,a]). % cudzÍCH
 srcp([í,c,h], [a,m,p,l]). % cudzÍCH
 srcp([í,c,h], [a,z,p,l]). % cudzÍCH
 srcp([í,c,h], [a,s,p,l]). % cudzÍCH
+% Koncovka = ich
+srcp([i,c,h], [a,m,p,g]). % cudzÍCH (r.k.)
+srcp([i,c,h], [a,z,p,g]). % cudzÍCH (r.k.)
+srcp([i,c,h], [a,s,p,g]). % cudzÍCH (r.k.)
+srcp([i,c,h], [a,m,p,a]). % cudzÍCH (r.k.)
+srcp([i,c,h], [a,m,p,l]). % cudzÍCH (r.k.)
+srcp([i,c,h], [a,z,p,l]). % cudzÍCH (r.k.)
+srcp([i,c,h], [a,s,p,l]). % cudzÍCH (r.k.)
 % Koncovka = ími
 srcp([í,m,i], [a,m,p,i]). % cudzÍMI
 srcp([í,m,i], [a,z,p,i]). % cudzÍMI
 srcp([í,m,i], [a,s,p,i]). % cudzÍMI
+% Koncovka = imi
+srcp([i,m,i], [a,m,p,i]). % cudzÍMI (r.k.)
+srcp([i,m,i], [a,z,p,i]). % cudzÍMI (r.k.)
+srcp([i,m,i], [a,s,p,i]). % cudzÍMI (r.k.)
 % Koncovka = []
 srcp([],[a,m,s,n]). % otcov
 srcp([],[a,m,s,a]). % otcov
-% Koncovka = a
-srcp([a], [a,z,s,n]). % otcovA
-% Koncovka = u
-srcp([u], [a,z,s,a]). % otcovU
 % Koncovka = o
 srcp([o], [a,s,s,n]). % otcovO
 srcp([o], [a,s,s,a]). % otcovO
-% Koncovka = i
-srcp([i], [a,m,p,n]). % otcovI
-% Koncovka = e
-srcp([e], [a,z,p,n]). % otcovE
-srcp([e], [a,s,p,n]). % otcovE
-srcp([e], [a,z,p,a]). % otcovE
-srcp([e], [a,s,p,a]). % otcovE
 % Koncovka = mu
 srcp([m,u], [a,m,s,d]). % otcovMU
 srcp([m,u], [a,s,s,d]). % otcovMU
@@ -246,31 +304,34 @@ srcp([h,o], [a,s,s,g]). % otcovHO
 
 
 
+% negace(+Kmen, +Slovo, -NoveSlovo, -Negace) :- ak zacina Slovo na "ne" a Kmen nie, nastaví Negace na 1 a ako NoveSlovo vrati Slovo bez "ne" na zaciatku, inak nastaví negace na 0 a NoveSlovo bude rovnaké ako Slovo.
+negace([X,Y|_], [n,a,j,N,E|SlovoRest], [n,a,j|SlovoRest], 1) :- X \= N, Y \= E, N = 'n', E = 'e', !.
+negace([n,e|KmenRest], [n,e|SlovoRest], [n,e|NoveSlovo], Neg) :- write(tady), negace(KmenRest, SlovoRest,NoveSlovo,Neg), !.
+negace([X,Y|_], [n,e|SlovoRest], SlovoRest, 1) :- X \= n, Y \= e, !.
+negace([X,Y|_], [n,e|SlovoRest], SlovoRest, 1) :- X \= n, Y = e, !.
+negace([X,Y|_], [n,e|SlovoRest], SlovoRest, 1) :- X = n, Y \= e, !.
+negace(_, Slovo, Slovo, 0).
+
+% ejs(+Kmen, +Slovo) :- po Kmeni nasleduje v Slove "ejš" alebo "š".
+ejs([], [š|_]) :- !.
+ejs([], [e,j,š|_]) :- !.
+ejs([K|RestKmen], [S|RestSlovo]) :- K = S, ejs(RestKmen, RestSlovo).
+
+% odoberEJS(+Kmen, +Slovo, -NoveSlovo) :- odoberie "ejš" zpoza kmeňa (aby som vedela správne určiť koncovku).
+odoberEJS([], [š|Rest], Rest) :- !.
+odoberEJS([], [e,j,š|Rest], Rest) :- !.
+odoberEJS([X|KmenRest], [Y|SlovoRest], [Y|Koncovka]) :- X = Y, odoberEJS(KmenRest, SlovoRest, Koncovka).
+
+% stupen(+Kmen, +Slovo, -Stupen, -NoveSlovo).
+stupen(Kmen, [n,a,j|SlovoRest], 3, NoveSlovo) :- ejs(Kmen, SlovoRest),
+						 odoberEJS(Kmen, SlovoRest, NoveSlovo), !.
+stupen(Kmen, Slovo, 2, NoveSlovo) :- ejs(Kmen, Slovo), odoberEJS(Kmen, Slovo, NoveSlovo), !.
+stupen(_,Slovo,1,Slovo).
+
 % koncovka(+Kmen, +Slovo, -Koncovka) :- ak odoberiem zo začiatku Slova Kmen, dostanem Koncovku.
 koncovka([], Koncovka, Koncovka).
 koncovka([X|_], [Y|_], [x]) :- X \= Y.
 koncovka([X|Xs], [X|Ys], Koncovka) :-  koncovka(Xs ,Ys, Koncovka).
-
-% negace(+Kmen, +Slovo, -NoveSlovo, -Negace) :- ak zacina Slovo na "ne" a Kmen nie, nastaví Negace na 1 a ako NoveSlovo vrati Slovo bez "ne" na zaciatku, inak nastaví negace na 0 a NoveSlovo bude rovnaké ako Slovo.
-negace([X,Y|_], [n,a,j,N,E|SlovoRest], [n,a,j|SlovoRest], 1) :- X \= N, Y \= E, N = 'n', E = 'e', !.
-negace([X,Y|_], [N,E|SlovoRest], SlovoRest, 1) :- X \= N, Y \= E, N = 'n', E = 'e', !.
-negace(_, Slovo, Slovo, 0).
-
-% ejs(+Kmen, +Slovo) :- po Kmeni nasleduje v Slove "ejš" alebo "š".
-ejs([], [š|_]).
-ejs([], [AaleboE,j,š|_]) :- AaleboE = a; AaleboE = e, !.
-ejs([K|RestKmen], [S|RestSlovo]) :- K = S, ejs(RestKmen, RestSlovo).
-
-% odoberEJS(+Kmen, +Slovo, -NoveSlovo) :- odoberie "ejš" zpoza kmeňa (aby som vedela správne určiť koncovku).
-odoberEJS([], [š|Rest], Rest).
-odoberEJS([], [AaleboE,j,š|Rest], Rest) :- AaleboE = a; AaleboE = e, !.
-odoberEJS([X|KmenRest], [Y|SlovoRest], [Y|Koncovka]) :- X = Y, odoberEJS(KmenRest, SlovoRest, Koncovka).
-
-% stupen(+Kmen, +Slovo, -Stupen, -NoveSlovo).
-stupen(Kmen, [N,A,J|SlovoRest], 3, NoveSlovo) :- N = 'n', A = 'a', J = 'j', ejs(Kmen, SlovoRest),
-						 odoberEJS(Kmen, SlovoRest, NoveSlovo), !.
-stupen(Kmen, Slovo, 2, NoveSlovo) :- ejs(Kmen, Slovo), odoberEJS(Kmen, Slovo, NoveSlovo), !.
-stupen(_,Slovo,1,Slovo).
 
 % znacka(+Kmen, +Slovo, -Znacka) :- vráti značku slova.
 znacka(Kmen, Slovo, [Negace, Stupen|Zvysok]) :- string_chars(Kmen, K), string_chars(Slovo,S),
